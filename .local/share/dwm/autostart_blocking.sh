e0ae6dcb701f2927cac887c2680e32c3d6500046
OnkarKunjir <onkarkunjir8@gmail.com>
2021-11-07 13:16:38 +0530

    Added script to add date text over wallpaper.

--- .local/share/dwm/autostart_blocking.sh
+++ .local/share/dwm/autostart_blocking.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # feh --bg-scale /home/onkar/Pictures/orange_theme.jpg
-nitrogen --restore
+# nitrogen --restore
+
+wallpaper_image="$HOME/Pictures/wallpapers/citynight.png"
+
 picom --experimental-backends &
 dunst &
 /home/onkar/.local/bin/dwm_bar &
 redshift &
+/home/onkar/.local/bin/wallpaper "$wallpaper_image" &
