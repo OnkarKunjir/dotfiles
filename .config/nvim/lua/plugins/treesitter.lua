d804e0438f5ed7d2e51ac6e4efa1a33fd29be136
OnkarKunjir <onkarkunjir8@gmail.com>
2026-03-28 19:21:48 +0530

    Adding hyper land configuration
    - Updating treesitter configuration

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,24 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
+    'nvim-treesitter/nvim-treesitter',
     branch = 'master',
     lazy = false,
-    build = ":TSUpdate",
-    opts = {
-        ensure_installed = { 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'c' },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-        },
-        indent = {
-            enable = true
-        },
-    }
+    build = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = { 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'c', 'python' },
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = {
+                enable = true
+            },
+        })
+
+        vim.wo.foldmethod = 'expr'
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.opt.foldenable = false
+        vim.opt.foldlevel = 20
+    end,
 }
