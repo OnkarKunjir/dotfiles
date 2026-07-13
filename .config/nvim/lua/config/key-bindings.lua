8441df8a436b04788ba5083f82bbe4e288409e16
OnkarKunjir <onkarkunjir8@gmail.com>
2026-07-13 11:30:54 +0530

    Fixing typo in lsp implementations binding

--- .config/nvim/lua/config/key-bindings.lua
+++ .config/nvim/lua/config/key-bindings.lua
@@ -54,7 +54,7 @@ end)
 vim.keymap.set('n', '<leader>ld', vim.lsp.buf.hover)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
-vim.keymap.set('n', 'g', vim.lsp.buf.implementation)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references)
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'LSP Code action' })
 vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, {
